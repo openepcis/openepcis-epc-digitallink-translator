1f60b1c4e32e335f6ceacb60d57db271fd1ce4fe
Devang Chauhan <devang.chauhan@benelog.com>
2025-06-17 19:06:21 +0530

    GEN-341; move DLPathNormalizer to epc link translator module

--- utils/src/main/java/io/openepcis/digitallink/model/ApplicationIdentifier.java
+++ utils/src/main/java/io/openepcis/digitallink/model/ApplicationIdentifier.java
@@ -1,66 +1,17 @@
 package io.openepcis.digitallink.model;
 
-import java.util.ArrayList;
+import lombok.Builder;
+import lombok.Data;
+
 import java.util.List;
 
 /**
  * Represents the properties of a GS1 Application Identifier.
  */
+@Data
+@Builder
 public class ApplicationIdentifier {
     final String title, label, shortcode, ai, format, type, checkDigit, regex;
     final boolean fixedLength;
     final List<String> qualifiers;
-
-    public ApplicationIdentifier(String title, String label, String shortcode, String ai, String format, String type, boolean fixedLength, String checkDigit, String regex, List<String> qualifiers) {
-        this.title = title;
-        this.label = label;
-        this.shortcode = shortcode;
-        this.ai = ai;
-        this.format = format;
-        this.type = type;
-        this.fixedLength = fixedLength;
-        this.checkDigit = checkDigit;
-        this.regex = regex;
-        this.qualifiers = qualifiers != null ? qualifiers : new ArrayList<>();
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public String getShortcode() {
-        return shortcode;
-    }
-
-    public String getAi() {
-        return ai;
-    }
-
-    public String getFormat() {
-        return format;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String getCheckDigit() {
-        return checkDigit;
-    }
-
-    public String getRegex() {
-        return regex;
-    }
-
-    public boolean isFixedLength() {
-        return fixedLength;
-    }
-
-    public List<String> getQualifiers() {
-        return qualifiers;
-    }
 }
