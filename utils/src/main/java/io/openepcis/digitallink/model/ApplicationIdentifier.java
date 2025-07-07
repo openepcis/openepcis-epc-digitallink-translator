b67f917be8fdf06ef9e6025201326f54b5c895df
Bhavesh Shah <bshah@benelog.com>
2025-07-07 17:36:13 +0530

    fix merge and compilation issues ... need to watchout for ApplicationIdentifier.java for awkward usage of lombok

--- utils/src/main/java/io/openepcis/digitallink/model/ApplicationIdentifier.java
+++ utils/src/main/java/io/openepcis/digitallink/model/ApplicationIdentifier.java
@@ -2,29 +2,74 @@ package io.openepcis.digitallink.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Data
+/**
+ * Represents the properties of a GS1 Application Identifier.
+ */
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class ApplicationIdentifier {
-    private String title;
-    private String label;
-    private String shortcode;
-    private String ai;
-    private String format;
-    private String type;
-    private Boolean fixedLength;
-    private String checkDigit;
-    private String regex;
-    private List<String> qualifiers;
+    final String title, label, shortcode, ai, format, type, checkDigit, regex;
+    final boolean fixedLength;
+    final List<String> qualifiers;
+
+    public ApplicationIdentifier(String title, String label, String shortcode, String ai, String format, String type, boolean fixedLength, String checkDigit, String regex, List<String> qualifiers) {
+        this.title = title;
+        this.label = label;
+        this.shortcode = shortcode;
+        this.ai = ai;
+        this.format = format;
+        this.type = type;
+        this.fixedLength = fixedLength;
+        this.checkDigit = checkDigit;
+        this.regex = regex;
+        this.qualifiers = qualifiers != null ? qualifiers : new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getShortcode() {
+        return shortcode;
+    }
+
+    public String getAi() {
+        return ai;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCheckDigit() {
+        return checkDigit;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
 
     public boolean isFixedLength() {
         return fixedLength;
     }
-}
 
+    public boolean getFixedLength() {
+        return fixedLength;
+    }
+
+    public List<String> getQualifiers() {
+        return qualifiers;
+    }
+}
